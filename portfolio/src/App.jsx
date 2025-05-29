eb9943cd6f3fda1ff86361967da4497d0c798b97
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-05-29 23:22:32 +0300

    updated

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -56,7 +56,7 @@ function App() {
             <div className="project-tile">
               <h3>Project 2</h3>
 
-              <p>A brief description of your project</p>
+              <p>A brief description of your project.</p>
               <Button variant="outlined" href="#">View Project</Button>
             </div>
           </div>
