5ead7711fef6aa6a7382a1e5c5590d55277b6ca6
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-07-15 00:00:03 +0300

    updated

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -97,6 +97,7 @@ function App() {
                   <Typography variant="body2">
                     A WordPress site for EESCL Uganda, showcasing CMS development skills.
                   </Typography>
+                  
                   <Button
                     variant="outlined"
                     href="https://eescluganda.wuaze.com/?i=1"
